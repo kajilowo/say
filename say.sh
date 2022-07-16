7eb9598d1adbee0bef165da83bee198fb280410d
kajilowo <109421926+kajilowo@users.noreply.github.com>
2022-07-17 05:28:39 +0700

    Add files via upload

---
+++ say.sh
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m' 
+green='\033[32;1m'
+ purple='\033[35;1m' 
+cyan='\033[36;1m'
+ red='\033[31;1m'
+ white='\033[37;1m' 
+yellow='\033[33;1m' 
+sleep 1
+echo
+toilet -f big -F gay by tukijo
+echo $green"selamat datang"
+echo
+sleep 2
+echo $yelow"silahkan pilih toolnya :"
+echo
+echo  $cyan"1.) pilih config"
+echo
+echo $blue"0.) keluar aja bro !!!"
+echo
+echo $white
+read -p "pilih mana : " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F guy by tukijo
+echo $green"config by tukijo"
+sleep 3
+echo $blue"https://sfile.mobi/jG66SWzS1y7"
+fi
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $blue
+figlet "tukijo"
+echo "tobe be contued"
+sleep 3
+exit
+fi
+
